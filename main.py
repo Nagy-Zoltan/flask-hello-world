a7b0c6e50b2d4b417b0f1c23b13273a21b0b092f
Zoltan Nagy <nagy.zoltan.con@hotstar.com>
2022-04-15 21:08:08 +0200

    add calculator

--- main.py
+++ main.py
@@ -9,5 +9,20 @@ def root():
     return 'Hello world!'
 
 
+@app.route('<operator>:string/<num1>:int/<num2>:int')
+def calculate(operator, num1, num2):
+    match operator:
+        case 'add':
+            return str(num1 + num2)
+        case 'sub':
+            return str(num1 - num2)
+        case 'mul':
+            return str(num1 * num2)
+        case 'div':
+            return str(num1 / num2)
+        case _:
+            return 'Invalid operation'
+
+
 if __name__ == '__main__':
     app.run()
